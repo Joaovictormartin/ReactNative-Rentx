a9b962e2459d9baf4e631a92bf81abe805c54cd9
Joao <joao.teixeira@aedb.br>
2021-12-26 22:37:13 -0300

    configurando o theme no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native'
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+import { 
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+
 import { Home } from './screens/Home';
+import theme from './styles/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
-    <Home/>
+    <ThemeProvider theme={theme}>
+      <Home/>
+    </ThemeProvider>
   );
 }
 
