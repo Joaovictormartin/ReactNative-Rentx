90d9685568cb0650b2deb68175b556cb45efb759
Joao <joao.teixeira@aedb.br>
2021-12-28 10:22:05 -0300

    component de imageSilder

---
+++ src/components/ImageSlider/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface imageIndexProps {
+  active?: boolean;
+}
+
+export const Container = styled.View`
+  width: 100%;
+`;
+
+export const ImageIndexes = styled.View`
+  flex-direction: row;
+  align-self: flex-end;
+  
+  padding-right: 24px;
+`;
+
+export const ImageIndex = styled.View<imageIndexProps>`
+  width: 6px;
+  height: 6px;
+
+  margin-left: 8px;
+  border-radius: 3px;
+
+  background-color: ${({ theme, active }) => active
+    ? theme.colors.title
+    : theme.colors.shape
+  } ;
+`;
+
+export const CarImageWrapper = styled.View`
+  width: ${Dimensions.get('window').width}px;
+  height: 132px;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CarImage = styled.Image`
+  width: 280px;
+  height: 132px;
+`;
+
+
