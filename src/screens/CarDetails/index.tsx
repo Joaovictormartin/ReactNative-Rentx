6ef7f670451404a80ef55a2cd34a288a794c5a1c
Joao <joao.teixeira@aedb.br>
2021-12-31 08:12:38 -0300

    configurando a rota da CarDetails para Sheduling  e passando Car selecionado como parâmetro

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+import { Cars } from "../../../interfaces/Cars";
+import { getAccessoryIcon } from "../../utils/getAccessoryIcon";
 
 import { Button } from "../../components/Button";
 import { Accessory } from "../../components/Accessory";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
-
-import SpeedSvg from '../../assets/svg/speed.svg';
-import ForceSvg from '../../assets/svg/force.svg';
-import PeopleSvg from '../../assets/svg/people.svg';
-import GasolineSvg from '../../assets/svg/gasoline.svg';
-import ExchangeSvg from '../../assets/svg/exchange.svg';
-import AccelerationSvg from '../../assets/svg/acceleration.svg';
 
 import {
   Container,
@@ -27,15 +23,20 @@ import {
   Price,
   Accesories,
   About,
-  Footer
+  Footer,
 } from "./styles";
 
-export function CarDetails() {
+interface Params {
+  car: Cars;
+}
 
+export function CarDetails() {
+  const route = useRoute();
+  const { car } = route.params as Params;
   const { navigate, goBack } = useNavigation();
 
   function handleSheduling() {
-    navigate('Sheduling');
+    navigate("Sheduling", { car });
   }
 
   return (
@@ -45,46 +46,37 @@ export function CarDetails() {
       </Header>
 
       <CarImages>
-        <ImageSlider 
-          imageUrl={["https://img2.gratispng.com/20180628/stg/kisspng-2018-audi-s5-3-0t-premium-plus-coupe-audi-rs5-2017-2018-audi-a5-coupe-5b35130451d959.0738564215302049323353.jpg"]}
-        />
+        <ImageSlider imageUrl={car.photos} />
       </CarImages>
 
       <Content>
         <Details>
           <Description>
-            <Brand>Marca</Brand>
-            <Name>Nome</Name>
+            <Brand>{car?.brand}</Brand>
+            <Name>{car?.name}</Name>
           </Description>
 
           <Rent>
-            <Period>Ao dia</Period>
-            <Price>R$ 580</Price>
+            <Period>{car?.rent?.period}</Period>
+            <Price>R$ {car?.rent?.price}</Price>
           </Rent>
         </Details>
 
         <Accesories>
-          <Accessory name="380km/h" icon={SpeedSvg}/>
-          <Accessory name="3.2s" icon={AccelerationSvg}/>
-          <Accessory name="800 HP" icon={ForceSvg}/>
-          <Accessory name="Gasolina" icon={GasolineSvg}/>
-          <Accessory name="Auto" icon={ExchangeSvg}/>
-          <Accessory name="2 pessoas" icon={PeopleSvg}/>
+          {car.accessories.map((accessory) => (
+            <Accessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={getAccessoryIcon(accessory.type)}
+            />
+          ))}
         </Accesories>
 
-        <About>
-          Este é automóvel desportivo. Surgiu do
-          lendário touro de lide indultado na praça Real 
-          Maestranza de Sevilla. É um belíssimo carro
-          para quem gosta de acelerar.
-        </About>
+        <About>{car?.about}</About>
       </Content>
 
       <Footer>
-        <Button 
-          title="Escolher período do aluguel"
-          onPress={handleSheduling}
-        />
+        <Button title="Escolher período do aluguel" onPress={handleSheduling} />
       </Footer>
     </Container>
   );
