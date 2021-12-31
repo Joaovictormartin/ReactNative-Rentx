82e26fc757a35f5251e0297a7dcad291803e7e93
Joao <joao.teixeira@aedb.br>
2021-12-31 08:14:18 -0300

    configurando a rota da Sheduling para ShedulingDetails e passando Car e data selecionada

--- src/screens/Sheduling/index.tsx
+++ src/screens/Sheduling/index.tsx
@@ -1,16 +1,24 @@
-import React from "react";
-import { StatusBar } from "react-native";
+import React, { useState } from "react";
+import { format } from 'date-fns';
+import { Alert, StatusBar } from "react-native";
 import { useTheme } from "styled-components";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-import { BackButton } from '../../components/BackButton';
-import { Calendar } from "../../components/Calendar";
-import { Button } from '../../components/Button';
-
-import Arrow from '../../assets/svg/arrow.svg'
-
+import { getPlatformDate } from "../../utils/getPlatformDate";
+import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
 import { 
-  Container, 
+  Calendar, 
+  DayProps,
+  MarkedDateProps,
+  generateInverval 
+} from "../../components/Calendar";
+
+import { Cars } from "../../../interfaces/Cars";
+import Arrow from "../../assets/svg/arrow.svg";
+
+import {
+  Container,
   Header,
   Title,
   RentalPeriod,
@@ -18,15 +26,60 @@ import {
   DateTitle,
   DateValue,
   Content,
-  Footter
+  Footter,
 } from "./styles";
 
+interface RentalPeriod {
+  startFormatted: string;
+  endFormatted: string;
+}
+
+interface Params {
+  car: Cars
+}
+
 export function Sheduling() {
+  const route = useRoute();
   const { colors } = useTheme();
+  const { car } = route.params as Params;
   const { navigate, goBack } = useNavigation();
 
+  const [ lastSelectedDate, setLastSelectedDate ] = useState<DayProps>({} as DayProps);
+  const [ markedDates, setMarkedDates ] = useState<MarkedDateProps>({} as MarkedDateProps);
+  const [ rentalPeriod, setRentalPeiod ] = useState<RentalPeriod>({} as RentalPeriod);
+
   function handleShedulingDetails() {
-    navigate('ShedulingDetails');
+    if(!rentalPeriod.startFormatted || !rentalPeriod.endFormatted) {
+      Alert.alert("Atenção", "Selecione o intervalo para alugar.")
+    } else {
+      navigate("ShedulingDetails", {
+        car,
+        dates: Object.keys(markedDates)
+      });
+    }
+  }
+
+  function handleChangeDate(date: DayProps) {
+    let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+    let end = date;
+
+    if(start.timestamp > end.timestamp) {
+      start = end;
+      end = start;
+    }
+
+    setLastSelectedDate(end);
+
+    const interval = generateInverval(start, end)
+    setMarkedDates(interval);
+
+    const firstDate = Object.keys(interval)[0];
+    const endDate = Object.keys(interval)[Object.keys(interval).length -1];
+
+    setRentalPeiod({
+      startFormatted: format(getPlatformDate(new Date(firstDate)), 'dd/MM/yyyy'),
+      endFormatted: format(getPlatformDate(new Date(endDate)), 'dd/MM/yyyy'),
+    });
   }
 
   return (
@@ -38,43 +91,40 @@ export function Sheduling() {
       />
 
       <Header>
-        <BackButton 
-          color={colors.shape}
-          onPress={() => goBack()}
-        />
+        <BackButton color={colors.shape} onPress={() => goBack()} />
 
         <Title>
-          Escolha uma {'\n'}
-          data de início e {'\n'}
-          fim do aluguel {'\n'}
+          Escolha uma {"\n"}
+          data de início e {"\n"}
+          fim do aluguel {"\n"}
         </Title>
 
         <RentalPeriod>
           <DateInfo>
             <DateTitle>De</DateTitle>
-            <DateValue selected={false}></DateValue>
+            <DateValue selected={!!rentalPeriod.startFormatted}>
+              {rentalPeriod.startFormatted}
+            </DateValue>
           </DateInfo>
 
           <Arrow width={48} height={10} />
 
           <DateInfo>
             <DateTitle>Até</DateTitle>
-            <DateValue selected={true}>19/06/2021</DateValue>
+            <DateValue selected={!!rentalPeriod.endFormatted}>
+              {rentalPeriod.endFormatted}
+            </DateValue>
           </DateInfo>
         </RentalPeriod>
       </Header>
 
       <Content>
-        <Calendar/>
+        <Calendar markedDates={markedDates} onDayPress={handleChangeDate} />
       </Content>
 
       <Footter>
-        <Button 
-          title="Confirmar"
-          onPress={handleShedulingDetails}
-        />
+        <Button title="Confirmar" onPress={handleShedulingDetails} />
       </Footter>
-
     </Container>
   );
 }
