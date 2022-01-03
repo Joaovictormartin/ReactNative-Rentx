9fe1feb31fbc33b9a9b64a65c03e7ef4fcc5fc25
Joao <joao.teixeira@aedb.br>
2022-01-02 23:31:23 -0300

    ajustando component Button - disabled

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,13 +6,21 @@ import { Container, Title } from "./styles";
 interface Props extends TouchableOpacityProps {
   title: string;
   color?: string;
+  disabledOpacity?: boolean
 }
 
-export function Button({ title, color, ...rest } : Props) {
+export function Button({ 
+  title, 
+  color, 
+  disabledOpacity, 
+  ...rest 
+} : Props) {
   return (
     <Container
       color={color}
-      activeOpacity={0.6} 
+      activeOpacity={0.6}
+      disabled={disabledOpacity}
+      style={{opacity: disabledOpacity ? 0.5 : 1}}
       {...rest}>
       <Title>{title}</Title>
     </Container>
