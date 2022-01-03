1a8d13c025c845dc159cce5946a156deb709c3cf
Joao <joao.teixeira@aedb.br>
2022-01-03 15:49:10 -0300

    add tela de Splash nas rotas

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "../screens/Home";
+import { Splash } from "../screens/Splash";
 import { MyCars } from "../screens/MyCars";
 import { Sheduling } from "../screens/Sheduling";
 import { CarDetails } from "../screens/CarDetails";
@@ -11,6 +12,7 @@ import { ShedulingComplete } from "../screens/ShedulingComplete";
 type RootStackParamList = {
   Home: any;
   MyCars: any;
+  Splash: any;
   Sheduling: any;
   CarDetails: any;
   ShedulingDetails: any;
@@ -21,11 +23,25 @@ const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export function StackRoutes() {
   return (
-    <Navigator>
-      <Screen name="Home" component={Home} options={{ headerShown: false }} />
+    <Navigator initialRouteName="Splash">
+      <Screen 
+        name="Home" 
+        component={Home} 
+        options={{ headerShown: false }} 
+      />
       <Screen
         name="Sheduling"
         component={Sheduling}
+        options={{ headerShown: false }}
+      />
+      <Screen
+        name="MyCars"
+        component={MyCars}
+        options={{ headerShown: false }}
+      />
+      <Screen
+        name="Splash"
+        component={Splash}
         options={{ headerShown: false }}
       />
       <Screen
@@ -41,11 +57,6 @@ export function StackRoutes() {
       <Screen
         name="ShedulingComplete"
         component={ShedulingComplete}
-        options={{ headerShown: false }}
-      />
-      <Screen
-        name="MyCars"
-        component={MyCars}
         options={{ headerShown: false }}
       />
     </Navigator>
