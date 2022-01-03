70e5c437e8fcb8d06f7ed5ab99396631cef30794
Joao <joao.teixeira@aedb.br>
2022-01-03 12:12:31 -0300

    configurando o slide de imagem

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { FlatList, ViewToken } from "react-native";
 
 import {
   Container,
@@ -12,22 +13,47 @@ interface Props {
   imageUrl: string[];
 }
 
-export function ImageSlider({ imageUrl } : Props) {
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
+export function ImageSlider({ imageUrl }: Props) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  //função para opter o index dentro da flatlist
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    setImageIndex(info.viewableItems[0].index!);
+  });
+
+  //função para renderizar o component de indicação da imagem
+  function ListImage() {
+    return imageUrl.map((_, index) => (
+      <ImageIndex key={index} active={index === imageIndex} />
+    ));
+  }
+
+  //função para renderizar o component da imagem
+  function renderItem({ item }: any) {
+    return (
+      <CarImageWrapper>
+        <CarImage source={{ uri: item }} resizeMode="cover" />
+      </CarImageWrapper>
+    );
+  }
+
   return (
     <Container>
-      <ImageIndexes>
-        <ImageIndex active={true}/>
-        <ImageIndex active={false}/>
-        <ImageIndex active={false}/>
-        <ImageIndex active={false}/>
-      </ImageIndexes>
+      <ImageIndexes>{ListImage()}</ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage
-          source={{ uri: imageUrl[0] }}
-          resizeMode="contain"
-        />
-      </CarImageWrapper>
+      <FlatList
+        data={imageUrl}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(key) => key}
+        renderItem={renderItem}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </Container>
   );
 }
