319c05579b1a893c835c83e83ddba726e4c5e5ad
Joao <joao.teixeira@aedb.br>
2022-01-10 01:07:54 -0300

    componen de Steps

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { ImageIndex } from "./styles";
+
+interface Props { active?: boolean }
+
+export function Bullet({ active = false } : Props) {
+  return <ImageIndex  active={active} />
+}
