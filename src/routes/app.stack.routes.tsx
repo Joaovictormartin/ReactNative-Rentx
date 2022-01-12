26e4e24e16a64f650f7485fe7c6e408b5064c2b3
Joao <joao.teixeira@aedb.br>
2022-01-12 20:15:05 -0300

    rotas depois da autenticação

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "../screens/Home";
-import { Splash } from "../screens/Splash";
 import { MyCars } from "../screens/MyCars";
 import { Sheduling } from "../screens/Sheduling";
 import { CarDetails } from "../screens/CarDetails";
@@ -12,7 +11,6 @@ import { Confirmation } from "../screens/Confirmation";
 type RootStackParamList = {
   Home: any;
   MyCars: any;
-  Splash: any;
   Sheduling: any;
   CarDetails: any;
   ShedulingDetails: any;
@@ -21,13 +19,12 @@ type RootStackParamList = {
 
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
-export function StackRoutes() {
+export function AppStackRoutes() {
   return (
     <Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={Home} options={{ gestureEnabled: false }}/>
       <Screen name="Sheduling" component={Sheduling} />
       <Screen name="MyCars" component={MyCars} />
-      <Screen name="Splash" component={Splash} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="ShedulingDetails" component={ShedulingDetails} />
       <Screen name="Confirmation" component={Confirmation} />
