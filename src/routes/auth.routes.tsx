04ef87e2d526df13d3b95d4aed801f8dc03d98e1
Joao <joao.teixeira@aedb.br>
2022-01-12 20:14:41 -0300

    rotas antes da autenticação

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { Splash } from "../screens/Splash";
 import { Signin } from "../screens/Signin";
 import { SignUpFirstStep } from "../screens/SignUp/SignUpFirstStep";
 import { SignUpSecondStep } from "../screens/SignUp/SignUpSecondStep";
 import { Confirmation } from "../screens/Confirmation";
 
 type RootStackParamList = {
+  Splash: any;
   Signin: any;
   SignUpFirstStep: any;
   SignUpSecondStep: any;
@@ -17,7 +19,8 @@ const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export function AuthRoutes() {
   return (
-    <Navigator initialRouteName="Signin" screenOptions={{ headerShown: false }}>
+    <Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+      <Screen name="Splash" component={Splash} />
       <Screen name="Signin" component={Signin} />
       <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
       <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
